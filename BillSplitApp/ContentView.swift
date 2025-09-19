25e3aa51ff622509c7d5641314caad867e110ff4
Yuki <yuki-kuwahara@hotmail.com>
2025-09-18 23:05:04 -0700

    Equal per person calculator done

--- BillSplitApp/ContentView.swift
+++ BillSplitApp/ContentView.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var billAmount: String = ""
+    @State private var tipPercentageUse: Bool = true
     @State private var tipPercentage: Double = 15
+    @State private var customTipAmount: String = ""
     @State private var numberOfPeople: Int = 2
     
     var total: Double {
         let bill = Double(billAmount) ?? 0 // nil-coalescing (if nil use 0)
-        let tip = bill * (tipPercentage / 100)
+        var tip: Double {
+            if tipPercentageUse {
+                return bill * (tipPercentage / 100)
+            } else {
+                return Double(customTipAmount) ?? 0
+            }
+        }
         return bill + tip
     }
     
@@ -23,40 +31,55 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack(spacing: 20) { // 20 points of space between each child view
-            Text("Bill Split Calculator")
-                .font(.largeTitle)
-            
-            TextField("Enter bill amount", text: $billAmount)
-                .keyboardType(.decimalPad)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-                .toolbar {
-                        ToolbarItemGroup(placement: .keyboard) { // Place it on keyboard
-                            Spacer() // Flexble space before the button, pushes it to the right
-                            Button("Done") {
-                                UIApplication.shared.sendAction( // Sending the action to the UI
-                                    #selector(UIResponder.resignFirstResponder), // Resign the first responder - the text field
-                                    to: nil, from: nil, for: nil // Send to any object that can respond
-                                )
+        NavigationStack {
+            Form {
+                Section(header: Text("Bill Details")) {
+                    TextField("Enter bill amount", text: $billAmount)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        .toolbar {
+                                ToolbarItemGroup(placement: .keyboard) { // Place it on keyboard
+                                    Spacer() // Flexble space before the button, pushes it to the right
+                                    Button("Done") {
+                                        UIApplication.shared.sendAction( // Sending the action to the UI
+                                            #selector(UIResponder.resignFirstResponder), // Resign the first responder - the text field
+                                            to: nil, from: nil, for: nil // Send to any object that can respond
+                                        )
+                                    }
+                                }
                             }
-                        }
+                    // The $billAmount here creates a binding effect to the @State so whenever
+                    // a user inputs an amount it will update in the UI)'
+                    Picker("Tip Type", selection: $tipPercentageUse) {
+                        Text("Percentage").tag(true)
+                        Text("Custom Amount").tag(false)
                     }
-            // The $billAmount here creates a binding effect to the @State so whenever
-            // a user inputs an amount it will update in the UI)'
-            
-            HStack {
-                Text("Tip: \(Int(tipPercentage))%")
-                Slider(value: $tipPercentage, in: 0...30, step: 1)
+                    .padding()
+                    
+                    if tipPercentageUse {
+                        HStack {
+                            Text("Tip: \(Int(tipPercentage))%")
+                            Slider(value: $tipPercentage, in: 0...30, step: 1)
+                        }
+                        .padding()
+                    } else {
+                        TextField("Enter custom tip", text: $customTipAmount)
+                            .keyboardType(.decimalPad)
+                    }
+                    
+                    Stepper("Total number of people: \(numberOfPeople)", value: $numberOfPeople, in: 1...20)
+                    
+                }
+                Section(header: Text("Results")) {
+                    Text("Total: $\(total, specifier: "%.2f")")
+                    Text("Per Person: $\(perPerson, specifier: "%.2f")")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                }
             }
-            .padding()
-            
-            Stepper("Total number of people: \(numberOfPeople)", value: $numberOfPeople, in: 1...20)
-            
-            Text("Total: $\(total, specifier: "%.2f")")
-            Text("Per Person: $\(perPerson, specifier: "%.2f")")
+            .navigationTitle("Tip Calculator")
         }
-        .padding()
     }
 }
 
